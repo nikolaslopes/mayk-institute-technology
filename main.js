5a86efe7a9d763999f889462b014089b64a866a7
Nix <nikolaslopes.dev@gmail.com>
2021-06-22 15:47:38 -0300

    add scroll

--- main.js
+++ main.js
@@ -18,3 +18,15 @@ for (const link of links) {
     nav.classList.remove('show')
   })
 }
+
+/* change header page when scroll is on */
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
+
+window.addEventListener('scroll', function () {
+  if (window.scrollY >= navHeight) {
+    header.classList.add('scroll')
+  } else {
+    header.classList.remove('scroll')
+  }
+})
