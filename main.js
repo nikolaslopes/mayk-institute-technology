3f1da14bac58593be65aea7e50fc456d14844997
Nix <nikolaslopes.dev@gmail.com>
2021-06-22 12:39:42 -0300

    add event hidden menu

--- main.js
+++ main.js
@@ -1,11 +1,20 @@
 // DOM -> Document Object Model
 
+/* open and close icon itens */
 const nav = document.querySelector('#header nav')
-
 const toggle = document.querySelectorAll('nav .toggle')
 
 for (const element of toggle) {
   element.addEventListener('click', function () {
     nav.classList.toggle('show')
+  })
+}
+
+/* event hidden menu */
+const links = document.querySelectorAll('nav ul li a')
+
+for (const link of links) {
+  link.addEventListener('click', function () {
+    nav.classList.remove('show')
   })
 }
