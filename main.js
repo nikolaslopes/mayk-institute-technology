b2ad264713eb23eac4fdcda46f8baad688b90ec4
Nix <nikolaslopes.dev@gmail.com>
2021-06-22 11:39:52 -0300

    add event listener

---
+++ main.js
@@ -0,0 +1,11 @@
+// DOM -> Document Object Model
+
+const nav = document.querySelector('#header nav')
+
+const toggle = document.querySelectorAll('nav .toggle')
+
+for (const element of toggle) {
+  element.addEventListener('click', function () {
+    nav.classList.toggle('show')
+  })
+}
