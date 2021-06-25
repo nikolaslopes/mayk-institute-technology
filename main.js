9bcd7e3133226911e620585b324127fe2deb0f7f
Nix <nikolaslopes.dev@gmail.com>
2021-06-25 03:33:00 -0300

    adjust swiper

--- main.js
+++ main.js
@@ -38,7 +38,13 @@ const swiper = new Swiper('.swiper-container', {
     el: '.swiper-pagination'
   },
   mousewheel: true,
-  keyboard: true
+  keyboard: true,
+  breakpoints: {
+    767: {
+      slidesPerView: 2,
+      setWrapperSize: true
+    }
+  }
 })
 
 /* SCROLL REVEAL */
