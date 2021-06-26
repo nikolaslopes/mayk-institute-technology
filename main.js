59af74963a9af8a3b2983ebdf6223124ddc77eb4
Nix <nikolaslopes.dev@gmail.com>
2021-06-26 01:38:23 -0300

    adjust const in main.js

--- main.js
+++ main.js
@@ -78,34 +78,8 @@ function backToTop() {
   }
 }
 
-/* activate menu shadow */
-const sections = document.querySelectorAll('main section[id]')
-function activateMenuAtCurrentSection() {
-  const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4
-
-  for (const section of sections) {
-    const sectionTop = section.offsetTop
-    const sectionHeight = section.offsetHeight
-    const sectionId = section.getAttribute('id')
-
-    const checkpointStart = checkpoint >= section
-    const checkpointEnd = checkpoint <= sectionTop + sectionHeight
-
-    if (checkpointStart && checkpointEnd) {
-      document
-        .querySelector('nav ul li a[href*=]' + sectionId + ']')
-        .classList.add('active')
-    } else {
-      document
-        .querySelector('nav ul li a[href*=]' + sectionId + ']')
-        .classList.remove('active')
-    }
-  }
-}
-
 /* scroll events */
 window.addEventListener('scroll', function () {
   changeHeaderWhenScoll()
   backToTop()
-  activateMenuAtCurrentSection
 })
